d4143157cac8f49e51ad79ca379d3cbbc0622ddf
VitorMScolari <vmarcals@hotmail.com>
2021-08-16 18:27:49 -0300

    added class book

---
+++ script.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+class Book{
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+function addBookToLibrary() {
+    myLibrary.push(Book);
+}
+
+function loopMyLibrary() {
+    myLibrary.map(book => {
+
+    })
+}
